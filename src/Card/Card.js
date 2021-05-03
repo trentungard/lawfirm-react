2db0c4526b06a6e789b33d3b32c7abc4c8b5a88a
trentungard <trentungard@gmail.com>
2021-05-02 21:03:24 -0400

    alt

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -8,14 +8,14 @@ const Card = ({className, src, name, title, shortDescription, longDescription}) 
 
     return isExpanded ? 
         <div className={className}>
-            <img src={`${src}`} alt="Photo of Chris"/>
+            <img src={`${src}`} alt=""/>
             <h3>{name}</h3>
             <h4>{title}</h4>
             <p>{longDescription}</p>
             <MdExpandMore className="rotated-carot" style={{transform: 'rotate(180deg)'}} size={30} fill="whitesmoke" onClick={() => setIsExpanded(!isExpanded)}/>
         </div>    
         :
-        <div className={className} alt="Photo of Matt">
+        <div className={className} alt="">
             <img src={`${src}`} />
             <h3>{name}</h3>
             <h4>{title}</h4>
